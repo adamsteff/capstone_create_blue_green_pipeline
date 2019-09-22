b7797906dc7d4ef002980810c5ad1fe964206141
adamsteff <adamsteff@adaptlx.com>
2019-09-23 08:16:12 +1000

    Merge remote-tracking branch 'remotes/origin/master' into green

--- app/index.php
+++ app/index.php
@@ -9,7 +9,6 @@ $variable = "Hello Udacity this is my green release!!! My name is Adam Steff";
 </head>
 <body style="color='green' text-align='center' ">
 <h1 style="color: green">
-
     <?php echo $variable;?>
 </h1>
 </body>
