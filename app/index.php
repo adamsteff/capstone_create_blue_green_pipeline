b699350596d07e9218830402fbb5a7283ba8f7ba
adamsteff <adamsteff@adaptlx.com>
2019-09-22 15:20:54 +1000

    Green branch

--- app/index.php
+++ app/index.php
@@ -5,7 +5,7 @@ $variable = "Hello World this is my green release";
 <!doctype html>
 <html>
 <head>
-    <title>Capstone project</title>
+    <title>Capstone project Updated</title>
 </head>
 <body style="color='green' text-align='center' ">
 <?php echo $variable;?>
