78daa3ff3e24047c87b80153b08b04b790dade7f
adamsteff <adamsteff@adaptlx.com>
2019-09-22 16:50:38 +1000

    Lint Fail

--- app/index.php
+++ app/index.php
@@ -2,7 +2,7 @@
 $variable = "Hello Udacity this is my blue release!!!";
 ?>
 
-<!doctype html>
+
 <html>
 <head>
     <title>Capstone project</title>
@@ -10,6 +10,6 @@ $variable = "Hello Udacity this is my blue release!!!";
 <body style="color='green' text-align='center' ">
 <h1 style="color: blue">
     <?php echo $variable;?>
-<</h1>
+</h1>
 </body>
 </html>
